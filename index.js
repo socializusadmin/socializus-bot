7a41c996cc82e1c06b6f80da1d47e69f18177e3a
vertingo <admin@vertin-go.com>
2019-12-04 06:05:11 +0100

    make it better

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ function handleMessage(sender_psid, received_message) {
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
 	
-	  response = {
+	  /*response = {
       "attachment": {
         "type": "template",
         "payload": {
@@ -135,11 +135,11 @@ function handleMessage(sender_psid, received_message) {
           }]
         }
       }
-    }
-	
-    /*response = {
-      "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
     }*/
+	
+    response = {
+      "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+    }
   } else if (received_message.attachments) {
     // Get the URL of the message attachment
     let attachment_url = received_message.attachments[0].payload.url;
